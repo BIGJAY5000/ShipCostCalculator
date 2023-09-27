11e4248d4b042018809eae58867337be3f121c44
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-27 09:15:18 -0400

    total

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+    //variable declarations
+        double price = 0;
+        System.out.print("Please enter the price of your item here:");
+       price = scan.nextDouble();
+        if (price >= 100) {
+            System.out.println("You have free shipping! Congratulations!");
+        }
+        else if (price < 100) {
+            System.out.println("You do not receive free shipping");
+            System.out.println("Your shipping cost is " + price*.02 + ", and your total price is " + price *1.02);
+        }
+
     }
 }
